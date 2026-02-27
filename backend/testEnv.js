f19c380ce413de838ea43cab6a7feb5a09d86fb4
cheran-hacker <cheransuresh869@gmail.com>
2026-02-27 14:57:03 +0530

    deploy

--- backend/testEnv.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-console.log('MONGO_URI loaded:', process.env.MONGO_URI ? 'Yes (length: ' + process.env.MONGO_URI.length + ')' : 'No');
-process.exit(0);
